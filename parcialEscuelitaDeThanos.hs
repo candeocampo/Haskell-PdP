dc1f575223641f51f2d50197c10880e585b81cdd
candeocampo <cocampo@frba.utn.edu.ar>
2024-05-22 15:33:16 -0300

    Parte2 parcial de Thanos

--- parcialEscuelitaDeThanos.hs
+++ parcialEscuelitaDeThanos.hs
@@ -43,7 +43,46 @@ energiaTotalUniverso :: Universo -> Float
 energiaTotalUniverso = sum.map energia.filter tieneMasDeUnaHabilidad
 --          universo = sum (map energia (filter tieneMasDeUnaHabilidad universo))
 
+--PARTE 2
 
+mente :: Float -> Gema
+mente = quitarEnergia
+
+quitarEnergia :: Float -> Gema 
+quitarEnergia valor personaje = personaje{energia=energia personaje - valor}
+
+alma :: String -> Gema 
+alma habilidadBuscar personaje = (quitarEnergia 10.eliminarHabilidad habilidadBuscar) personaje
+
+eliminarHabilidad :: String -> Gema
+eliminarHabilidad habilidad personaje = personaje{habilidades=filter(/=habilidad)(habilidades personaje)}
+
+espacio :: String -> Gema
+espacio planetaNuevo personaje = (quitarEnergia 20.transportarRival planetaNuevo) personaje 
+
+transportarRival :: String -> Gema
+transportarRival nuevoPlaneta personaje = personaje{planetaQueVive=nuevoPlaneta}
+
+poder :: Gema
+poder personaje 
+    | cantidadHabilidadesPersonaje personaje = personaje{energia=0,habilidades=[]}
+    | otherwise = personaje
+
+cantidadHabilidadesPersonaje :: Personaje -> Bool
+cantidadHabilidadesPersonaje personaje = length (habilidades personaje) <=2
+
+tiempo :: Gema
+tiempo personaje = (quitarEnergia 50.reducirEdad) personaje
+
+reducirEdad ::  Personaje -> Personaje
+reducirEdad personaje 
+    | edad personaje `div` 2 < 18 = personaje{edad=18}
+    | otherwise = personaje{edad=edad personaje `div` 2}
+
+gemaLoca :: Gema -> Gema
+gemaLoca gema = gema.gema
+
+--PUNTO 3)
 
 
 
