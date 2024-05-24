095804b3e1b043c2c4ce69b7ac2e5921c4f9ad3a
candeocampo <cocampo@frba.utn.edu.ar>
2024-05-24 19:05:47 -0300

    Subo punto 2 de Vacaciones.hs

--- parcialVacaciones.hs
+++ parcialVacaciones.hs
@@ -6,20 +6,6 @@ data Turista = Turista{
     idiomasQueHabla :: [Idioma]
 }deriving(Show,Eq)
 
---Ejemplos 
-ana :: Turista
-ana =
-  Turista { cansancio = 0 , stress = 20, viajaSolo = False, idiomasQueHabla = ["espaniol"] }
-
-beto :: Turista
-beto =
-  Turista { cansancio = 15, stress = 15, viajaSolo = True, idiomasQueHabla = ["aleman"] }
-
-cathi :: Turista
-cathi =
-  Turista { cansancio = 15, stress = 15, viajaSolo = True, idiomasQueHabla = ["aleman", "catalan"] }
-
-
 type Idioma = String
 
 type Excursion = Turista -> Turista
@@ -28,10 +14,13 @@ irALaPlaya :: Excursion
 irALaPlaya = bajarCansansio 5.bajarStress 1
 
 bajarCansansio :: Int -> Excursion
-bajarCansansio valor turista = turista{cansancio=cansancio turista - valor}
+bajarCansansio valor turista = turista{cansancio=cansancio turista + valor}
 
 bajarStress :: Int -> Excursion
 bajarStress num turista = turista{stress=stress turista-num}
+
+cambiarStressPorcentual :: Int -> Turista -> Turista
+cambiarStressPorcentual porciento turista = bajarStress (div (porciento * stress turista) 100) turista
 
 apreciarElementoDelPaisaje :: String -> Excursion
 apreciarElementoDelPaisaje elemento = bajarStress (length elemento) 
@@ -56,12 +45,49 @@ aumentarCansansio valor turista = turista{cansancio=cansancio turista + valor}
 
 paseoEnBarco :: String -> Excursion
 paseoEnBarco marea turista
-    | marea == "fuerte" = (bajarStress (-6).aumentarCansansio (10)) turista
+    | marea == "fuerte" = (bajarStress 6.aumentarCansansio 10) turista
     | marea == "moderada" = turista
     | marea == "tranquila" = (caminar 10 . apreciarElementoDelPaisaje "mar" . salirAHablarIdioma "aleman") turista
 
+--PUNTO 1
 
+ana :: Turista
+ana =
+  Turista { cansancio = 0 , stress = 20, viajaSolo = False, idiomasQueHabla = ["espaniol"] }
 
+beto :: Turista
+beto =
+  Turista { cansancio = 15, stress = 15, viajaSolo = True, idiomasQueHabla = ["aleman"] }
+
+cathi :: Turista
+cathi =
+  Turista { cansancio = 15, stress = 15, viajaSolo = True, idiomasQueHabla = ["aleman", "catalan"] }
+
+--PUNTO 2a)
+hacerExcursion :: Excursion -> Turista -> Turista  
+hacerExcursion excursion turista  = (cambiarStressPorcentual (10).excursion) turista
+
+--PUNTO 2b)
+deltaSegun :: (a -> Int) -> a -> a -> Int
+deltaSegun f algo1 algo2 = f algo1 - f algo2
+
+deltaExcursionSegun :: (Turista -> Int)-> Turista -> Excursion -> Int
+deltaExcursionSegun funcion turista excursion = deltaSegun funcion (hacerExcursion excursion turista) turista
+
+--PUNTO 2c)
+--i)
+
+excursionEducativa :: Turista -> Excursion -> Bool
+excursionEducativa turista = (>0).deltaExcursionSegun (length.idiomasQueHabla) turista
+
+--ii)
+excursionesDesestresantes :: Turista -> [Excursion] -> [Excursion]
+excursionesDesestresantes turista listaExcursiones = filter (esDesestresante turista) listaExcursiones
+
+esDesestresante :: Turista -> Excursion -> Bool
+esDesestresante turista = (<= -3) . deltaExcursionSegun stress turista
+
+--PARTE 3)
 
 
 
