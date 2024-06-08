c981201281dcad304490897471e2891c9d706bb3
candeocampo <cocampo@frba.utn.edu.ar>
2024-06-08 19:09:46 -0300

    Nueva version de parcial funcional 2023

--- parcialFuncional.hs
+++ parcialFuncional.hs
@@ -1,6 +1,5 @@
 
-
-data Investigador = Investigador {
+data Investigador = Investigador{
     nombre :: String,
     cordura :: Int,
     items :: [Item],
@@ -12,95 +11,76 @@ data Item = Item{
     valor :: Int
 }deriving (Show,Eq)
 
-
 maximoSegun f = foldl1 (mayorSegun f)
 
-{- mayorSegun f a b 
-    | f a > f b = a 
+mayorSegun :: (Investigador -> Int) -> Investigador -> Investigador -> Investigador
+mayorSegun f a b 
+    | f a > f b = a
     | otherwise = b
--}
-
---Casos de Prueba
-investigador1 :: Investigador
-investigador1  = Investigador "Candela" 60 [item1] ["Perder","Morir"]
-
-investigador2 :: Investigador 
-investigador2 = Investigador "Tomas" 30 [item1,item2] ["Ganar","Perder"]
-
-investigador3 :: Investigador
-investigador3 = Investigador "Morena" 0 [] ["Jugar"]
-
-item1 :: Item
-item1 = Item "Libros Fisica" 150
-
-item2 :: Item
-item2 = Item "Fisica cuantica" 350
-
-lista1 :: [Investigador]
-lista1 = [investigador1,investigador2,investigador3]
-
-lista2 :: [Investigador]
-lista2 = [investigador1]
-
-----------------
--- Resolución --
-----------------
 
 --1.a)
-enloquezca :: Int -> Investigador -> Investigador
-enloquezca valor investigador = investigador{cordura=dominioDeCordura (cordura investigador) valor}
+modificarCordura :: (Int -> Int) -> Investigador -> Investigador
+modificarCordura modificacion investigador = investigador{cordura= (modificacion.cordura) investigador}
 
-dominioDeCordura :: Int -> Int -> Int
-dominioDeCordura valor variacion
-    | (>0)(valor + variacion) = valor + variacion
-    | otherwise = 0
+enloquezca :: Int -> Investigador -> Investigador
+enloquezca puntos = modificarCordura (max 0 . flip (-) puntos)
 
 --1.b)
 hallarItem :: Item -> Investigador -> Investigador
-hallarItem agregarItem = enloquezca (valor agregarItem) . (incorporarItem agregarItem)
+hallarItem  itemNuevo = enloquezca (valor itemNuevo) . incorporarItem itemNuevo
 
 incorporarItem :: Item -> Investigador -> Investigador
-incorporarItem buscarItem investigador = investigador{items=buscarItem:items investigador}
+incorporarItem itemNuevo investigador = investigador{items=itemNuevo:items investigador}
 
 --2)
-tieneItem :: Item -> [Investigador] -> Bool
-tieneItem itemBuscar investigadores = any (nombreIndicado itemBuscar) investigadores
+tieneItemAlgunInvestigador :: String -> [Investigador] -> Bool
+tieneItemAlgunInvestigador nomItem = any (tieneItemInvestigador nomItem) 
 
-nombreIndicado :: Item -> Investigador -> Bool
-nombreIndicado item investigador = any (esMismoNombre item) (items investigador)
-
-esMismoNombre :: Item -> Item -> Bool
-esMismoNombre item1 item2 = nombreItem item1 == nombreItem item2
+tieneItemInvestigador :: String -> Investigador -> Bool
+tieneItemInvestigador nomItem  = elem nomItem . map nombreItem . items
 
 --3)
-liderInvestigador' :: [Investigador] -> Investigador
-liderInvestigador' [investigador] = investigador
-liderInvestigador' (investigador1:investigador2:listaInvestigadores) = mayorSegun potencialInvestigador investigador1 investigador2
+lider :: [Investigador] -> Investigador
+lider investigadores = maximoSegun potencialInvestigador investigadores
 
-liderInvestigador :: [Investigador] -> Investigador
-liderInvestigador investigadores = maximoSegun potencialInvestigador investigadores
-
-
-mayorSegun f a b 
-    | cordura (f a) > cordura (f b) = a 
-    | otherwise = b
-
-potencialInvestigador :: Investigador -> Investigador
+potencialInvestigador :: Investigador -> Int
 potencialInvestigador investigador 
-    | (==0) (cordura investigador)= investigador{cordura=0}
-    | otherwise = investigador{cordura=cordura investigador * (experiencia investigador) + maximoItems investigador}
+    | not(estaLoco investigador) = aplicarPotencial investigador
+    | otherwise = 0
 
-maximoItems :: Investigador -> Int
-maximoItems investigador = maximum(map valor (items investigador))
+aplicarPotencial :: Investigador -> Int 
+aplicarPotencial investigador = (cordura investigador) * experienciaInvestigador investigador + valorMaximoitems (items investigador)
 
-experiencia :: Investigador -> Int
-experiencia investigador = 1 + 3 * length (sucesosEvitados investigador)
+experienciaInvestigador :: Investigador -> Int
+experienciaInvestigador = (1+).(3*).length.sucesosEvitados 
+
+estaLoco :: Investigador -> Bool
+estaLoco investigador = 0 == cordura investigador
+
+valorMaximoitems :: [Item] -> Int
+valorMaximoitems [] = 0
+valorMaximoitems items = maximum  (map valor items)
+
+
+--4) usar deltaSegun
+
+deltaSegun ponderacion transformacion valor = abs ((ponderacion.transformacion) valor - ponderacion valor)
 
 --4.a)
+deltaEnLaCorduraTotal :: Int -> [Investigador] -> Int 
+deltaEnLaCorduraTotal puntos =  sum. map (deltaSegun cordura (enloquezca puntos)) 
 
-deltaSegun ponderacion transformacion valor = abs ((ponderacion . transformacion) valor - ponderacion valor)
+--4.b)
 
-deltaSegunCordura cordura enloquezca valor = abs ((cordura . enloquezca) valor - cordura valor)
+
+
+
+
+
+
+
+
+
 
 
 
